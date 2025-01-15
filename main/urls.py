a1e9f7d86608c99e72799efd7c458b0fcf2f5a73
Voenkomp <remesey97@gmail.com>
2025-01-16 08:43:25 +1000

    change style

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("about/", views.about),
+    path("", views.index, name="home"),
+    path("add/", views.addprinter, name="addprinter"),
+    path("about/", views.about, name="about"),
 ]
--- main/views.py
+++ main/views.py
@@ -6,5 +6,9 @@ def index(request):
     return render(request, "main/index.html")
 
 
+def addprinter(request):
+    return render(request, "main/addprinter.html")
+
+
 def about(request):
     return render(request, "main/about.html")
