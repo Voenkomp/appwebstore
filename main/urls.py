8eae514a81588cc80cf9b7f2b0c852acaf3003bd
Voenkomp <remesey97@gmail.com>
2025-01-22 17:23:10 +1000

    transform app page

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="home"),
     path("about/", views.about, name="about"),
+    path("device/<int:pk>", views.DeviceDetailView.as_view(), name="device-detail"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from adddevice.models import Device
+from django.views.generic import DetailView
 
 
 def index(request):
     device = Device.objects.order_by("inv_num")
     return render(request, "main/index.html", {"device": device})
+
+
+class DeviceDetailView(DetailView):
+    model = Device
+    template_name = "main/detail_device.html"
+    context_object_name = "printer"
 
 
 def about(request):
