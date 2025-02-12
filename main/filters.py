cee5f5e4906a0d74725aa7eb92a6b3a8c1ef8f3f
Voenkomp <remesey97@gmail.com>
2025-02-12 17:29:29 +1000

    change all-device page

--- main/filters.py
+++ main/filters.py
@@ -1,14 +1,69 @@
 import django_filters
-from .models import Device
-from django.forms import ModelForm, TextInput
+from django.forms import TextInput
 
 
 class DeviceFilter(django_filters.FilterSet):
     inv_num = django_filters.CharFilter(
+        field_name="inv_num",
         lookup_expr="icontains",
-        label="Инвентарный номер",
+        label="",
+        widget=TextInput(
+            attrs={
+                "class": "form-control form-filter",
+                "placeholder": "Инвентарный номер",
+            }
+        ),
     )
 
-    class Meta:
-        model = Device
-        fields = ["inv_num"]
+    serial = django_filters.CharFilter(
+        lookup_expr="icontains",
+        label="",
+        widget=TextInput(
+            attrs={"class": "form-control form-filter", "placeholder": "Серийный номер"}
+        ),
+    )
+
+    prod_mod_dev = django_filters.CharFilter(
+        field_name="prod_mod_dev__producer_model",
+        lookup_expr="icontains",
+        label="",
+        widget=TextInput(
+            attrs={"class": "form-control form-filter", "placeholder": "Модель"}
+        ),
+    )
+
+    building = django_filters.CharFilter(
+        lookup_expr="icontains",
+        label="",
+        widget=TextInput(
+            attrs={"class": "form-control form-filter", "placeholder": "Здание"}
+        ),
+    )
+
+    location = django_filters.CharFilter(
+        lookup_expr="icontains",
+        label="",
+        widget=TextInput(
+            attrs={"class": "form-control form-filter", "placeholder": "Расположение"}
+        ),
+    )
+
+    hostname = django_filters.CharFilter(
+        lookup_expr="icontains",
+        label="",
+        widget=TextInput(
+            attrs={"class": "form-control form-filter", "placeholder": "Hostname"}
+        ),
+    )
+
+    ip_add = django_filters.CharFilter(
+        lookup_expr="icontains",
+        label="",
+        widget=TextInput(
+            attrs={"class": "form-control form-filter", "placeholder": "IP-адрес"}
+        ),
+    )
+
+    # class Meta:
+    #     model = Device
+    #     fields = ["inv_num", "serial", "prod_mod_dev", "building"]
